583b05129bdbc81934b8cda916b3ca0371c29fc3
Bw3ll <49998815+Bw3ll@users.noreply.github.com>
2026-03-15 22:47:06 -0500

    Add files via upload

--- shellWasp.py
+++ shellWasp.py
@@ -1,7 +1,6 @@
-# from .start import bramwell_syscall
 import start.shellWasp
 
 from start.shellWasp import *
 if __name__ == "__main__":
-	print ("hi. i am an evil wasp.")
+	print ("hi. i am an evil wasp.\n")
 	syscallMain()
